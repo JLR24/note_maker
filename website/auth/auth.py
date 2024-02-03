c91de1c0ed7fb1670a1e52e132901e78a3cf58d2
Jack Ricketts <95173538+JLR24@users.noreply.github.com>
2024-02-03 21:19:29 +0000

    Added navbar and login page.

--- website/auth/auth.py
+++ website/auth/auth.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, request, flash, render_template, redirect, url_for
-from flask_login import current_user, login_required
+from flask_login import current_user, login_required, login_user, logout_user
+from werkzeug.security import check_password_hash
+from ..models import db, User
 
 auth = Blueprint("auth", __name__, template_folder="templates", static_folder="static")
 
@@ -7,7 +9,15 @@ auth = Blueprint("auth", __name__, template_folder="templates", static_folder="s
 @auth.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
-        pass
+        # Query the database for a user of the matching username
+        user = User.query.filter(User.username == request.form.get("username") and check_password_hash(User.password, request.form.get("password"))).first()
+        if user:
+            login_user(user, remember=True)
+            flash(f"Welcome back, {user.username}!", category="success")
+            return redirect(url_for("home.home"))
+        else:
+            flash("Invalid login details.", category="error")
+            return redirect(url_for("auth.login"))
     return render_template("login.html", user=current_user)
 
 
